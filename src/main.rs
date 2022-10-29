46e82fb338461e39233b98b4205a0b8fa5eebc45
Ricky <ritrix@gmail.com>
2022-10-29 13:05:18 +0200

    Dronecito

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
 use bevy::prelude::*;
-use bevy_rapier3d::prelude::*;
+use bevy_rapier3d::{prelude::*, rapier::prelude::{ColliderSet, ColliderBuilder, RigidBodyBuilder, ColliderMassProps}};
+use smooth_bevy_cameras::{
+    controllers::unreal::{UnrealCameraBundle, UnrealCameraController, UnrealCameraPlugin},
+    LookTransformPlugin,
+};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugin(RapierDebugRenderPlugin::default())
-        .add_startup_system(setup_graphics)
+        .add_plugin(LookTransformPlugin)
+        .add_plugin(UnrealCameraPlugin::default())
+        .add_startup_system(setup_camera)
         .add_startup_system(setup_physics)
         .add_system(print_ball_altitude)
         .run();
 }
 
-fn setup_graphics(mut commands: Commands) {
-    // Add a camera so we can see the debug-render.
-
-    commands.spawn_bundle(Camera3dBundle {
-        transform: Transform::from_xyz(-3.0, 3.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..Default::default()
-    });
-    // commands.spawn_bundle(PerspectiveCameraBundle {
-    //     transform: Transform::from_xyz(-3.0, 3.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y), ..Default::default()
-    // });
+fn setup_camera(mut commands: Commands) {
+    commands
+        .spawn_bundle(Camera3dBundle::default())
+        .insert_bundle(UnrealCameraBundle::new(
+            UnrealCameraController::default(),
+            Vec3::new(-3.0, 3.0, 10.0),
+            Vec3::new(0., 0., 0.),
+        ));
 }
 
 fn setup_physics(mut commands: Commands) {
@@ -31,13 +35,41 @@ fn setup_physics(mut commands: Commands) {
         .insert(Collider::cuboid(100.0, 0.1, 100.0))
         .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, -2.0, 0.0)));
 
+    let drone_width = 0.75;
+    let drone_height = 0.1;
+
     /* Create the bouncing ball. */
     commands
         .spawn()
         .insert(RigidBody::Dynamic)
-        .insert(Collider::ball(0.5))
-        .insert(Restitution::coefficient(0.7))
-        .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, 4.0, 0.0)));
+        .with_children(|children| {
+            children.spawn()
+                .insert(Collider::cuboid(drone_width, drone_height, drone_height));
+            children.spawn()
+                .insert(Collider::cuboid(drone_height, drone_height, drone_width));
+            children.spawn()
+                .insert(Collider::cylinder(drone_height, drone_height))
+                // Position the collider relative to the rigid-body.
+                .insert_bundle(TransformBundle::from(Transform::from_xyz(drone_width, 0.0, 0.0)));
+            children.spawn()
+                .insert(Collider::cylinder(drone_height, drone_height))
+                // Position the collider relative to the rigid-body.
+                .insert_bundle(TransformBundle::from(Transform::from_xyz(-drone_width, 0.0, 0.0)));
+            children.spawn()
+                .insert(Collider::cylinder(drone_height, drone_height))
+                // Position the collider relative to the rigid-body.
+                .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, 0.0, drone_width)));
+            children.spawn()
+                .insert(Collider::cylinder(drone_height, drone_height))
+                // Position the collider relative to the rigid-body.
+                .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, 0.0, -drone_width)));
+        })
+        // .insert(Restitution::coefficient(0.7))
+        // .insert(ExternalForce {
+        //     force: Vec3::new(1.0, 0.0, 0.0),
+        //     torque: Vec3::new(1.0, 0.0, 0.0),
+        // })
+        .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, 10.0, 0.0)));
 }
 
 fn print_ball_altitude(positions: Query<&Transform, With<RigidBody>>) {
